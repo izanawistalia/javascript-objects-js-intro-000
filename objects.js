e1c7e9d503413ea73c4922295408aee286af0fb6
rihan <dikshabelwal0@gmail.com>
2019-04-14 04:26:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ function updatePlaylist(obj, key, value){
   return Object.assign(obj, {[key]: value});
 }
 function removeFromPlaylist(obj, key){
-  delete obj.key ;
+  delete obj[key] ;
   return obj ;
 }
 var playlist = { matsushitaYuya : 'bird'}
